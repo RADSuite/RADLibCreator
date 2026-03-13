b100ed46c229da978f869c6f5e3de1a1f2d84a04
Remington Manning-Neal <nealr@login01.rc.byu.edu>
2026-03-12 20:46:05 -0600

    Updated sql script ti filter out malformed RefSeq accessions

--- workflows/scripts/load_refseq_db.sql
+++ workflows/scripts/load_refseq_db.sql
@@ -1,12 +1,21 @@
 .mode tabs
 .import temp.tsv all_data
 
+-- Make sure all data in Assembly Accession are accessions
+CREATE TABLE filtered_data AS
+SELECT *
+FROM all_data
+WHERE "Assembly Accession" LIKE 'GCF%'
+AND "Assembly Accession" IS NOT NULL;
+
+
 CREATE TABLE accessionTaxa AS
 SELECT DISTINCT "Organism Taxonomic ID" AS "taxa", "Assembly Accession" AS "accession"
-FROM all_data;
+FROM filtered_data;
 
 CREATE TABLE names AS
 SELECT DISTINCT "Organism Taxonomic ID" AS "id", "Organism name" AS "name"
-FROM all_data;
+FROM filtered_data;
 
 DROP TABLE all_data;
+DROP TABLE filtered_data;
